b03091e0515e468c6de8e3ca0e174f216fff921a
Anastasiya <anastasiya.klyap@gmail.com>
2023-05-03 11:06:47 +0200

    reformat

--- script.ts
+++ script.ts
@@ -9,8 +9,8 @@ const numInput = document.querySelector('.num-input') as HTMLInputElement;
 
 const persons: string[] = [];
 function addPerson(firstName: string, lastName: string) {
-	const name: string = firstName + lastName;
-	persons.push(name);
+  const name: string = firstName + lastName;
+  persons.push(name);
 }
 
 let firstName: string = 'John';
@@ -18,11 +18,11 @@ let lastName: string = 'Doe';
 let age: number = 18;
 
 const person: {
-	firstName: string;
-	lastName: string;
+  firstName: string;
+  lastName: string;
 } = {
-	firstName,
-	lastName,
+  firstName,
+  lastName,
 };
 
 addPerson(person.firstName, person.lastName);
@@ -30,85 +30,85 @@ addPerson('Amanda', 'Doe');
 addPerson('Thomas', 'Jefferson');
 
 enum Roles {
-	Admin = 1,
-	Moderator = 2,
-	User = 3,
+  Admin = 1,
+  Moderator = 2,
+  User = 3,
 }
 
 interface person {
-	id: number | string;
-	firstName: string;
-	lastName: string;
-	role: number;
+  id: number | string;
+  firstName: string;
+  lastName: string;
+  role: number;
 }
 
 const JohnDoe: person = {
-	id: 1,
-	firstName: 'John',
-	lastName: 'Doe',
-	role: Roles.Admin,
+  id: 1,
+  firstName: 'John',
+  lastName: 'Doe',
+  role: Roles.Admin,
 };
 
 const AmandaDoe = {
-	id: 2,
-	firstName: 'John',
-	lastName: 'Doe',
-	role: Roles.Moderator,
+  id: 2,
+  firstName: 'John',
+  lastName: 'Doe',
+  role: Roles.Moderator,
 };
 
 const ThomasJefferson = {
-	id: 'rwe5345sfst3453543',
-	firstName: 'John',
-	lastName: 'Doe',
-	role: Roles.User,
+  id: 'rwe5345sfst3453543',
+  firstName: 'John',
+  lastName: 'Doe',
+  role: Roles.User,
 };
 
 const names: [string, string, string] = [
-	JohnDoe.firstName,
-	AmandaDoe.firstName,
-	ThomasJefferson.firstName,
+  JohnDoe.firstName,
+  AmandaDoe.firstName,
+  ThomasJefferson.firstName,
 ];
 const JohnDoeSummary: [string, string, number] = [
-	JohnDoe.firstName,
-	JohnDoe.lastName,
-	JohnDoe.role,
+  JohnDoe.firstName,
+  JohnDoe.lastName,
+  JohnDoe.role,
 ];
 
 function join(valOne: string | number, valTwo: string | number) {
-	if (typeof valOne === 'number' && typeof valTwo === 'number') {
-		return valOne + valTwo;
-	} else if (typeof valOne === 'string' && typeof valTwo === 'string') {
-		return valOne + valTwo;
-	} else {
-		return false;
-	}
+  if (typeof valOne === 'number' && typeof valTwo === 'number') {
+    return valOne + valTwo;
+  } else if (typeof valOne === 'string' && typeof valTwo === 'string') {
+    return valOne + valTwo;
+  } else {
+    return false;
+  }
 }
 
 function showMyName(name: string): void {
-	console.log(name);
+  console.log(name);
 }
 
 showMyName('John');
 
 function square(a: number): number {
-	return a * a;
+  return a * a;
 }
 
 square(10);
 
 function sum(title: string, ...numbers: number[]): string {
-	return title + ' = ' + numbers.reduce((sum, num) => sum + num, 0);
+  return title + ' = ' + numbers.reduce((sum, num) => sum + num, 0);
 }
 
 sum('Numbers', 1, 6, 10);
 
 const executeFunc = (
-	func: (title: string, age: number, hobbies: string[]) => boolean
+  func: (title: string, age: number, hobbies: string[]) => boolean
 ): void => {
-	const title: string = 'Lorem Ipsum';
-	func(title, 24, ['Sport', 'Movies', 'Soccer']);
+  const title: string = 'Lorem Ipsum';
+  func(title, 24, ['Sport', 'Movies', 'Soccer']);
 };
 
 executeFunc((title, age, hobbies) => {
-	return true;
+  return true;
 });
